e2677e6b1b88f40abdc2761dd57d7dbb84ac188f
dchay <dylanhay@rogers.com>
2022-09-10 13:33:19 -0700

    thoughts array and virtual added to User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,36 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [],
   },
-  email: {
-    type: String,
-  },
-  thoughts: [],
-  friends: []
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of thoughts and replies on retrieval
+UserSchema.virtual("thoughtCount").get(function () {
+  return this.thoughts.length;
 });
 
 // create the User model using the UserSchema
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 // export the User model
 module.exports = User;
