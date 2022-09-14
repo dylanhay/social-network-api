af62d0794e0a914d01ce3043e3d3364ff770dbc2
dchay <dylanhay@rogers.com>
2022-09-13 19:21:29 -0700

    Thought Get All route added

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,6 +1,24 @@
 const { Thought, User } = require("../models");
 
+// .populate({
+//   path: "thoughts",
+//   select: "-__v",
+// })
+
+
 const thoughtController = {
+  // get all thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  
   // add thought
   addThought({ params, body }, res) {
     console.log(body);
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 const {
+  getAllThought,
   addThought,
   removeThought,
   addReaction,
   removeReaction,
 } = require("../../controllers/thought-controller");
+
+// /api/thoughts
+router.route("/").get(getAllThought);
 
 // /api/thoughts/<userId>
 router.route("/:userId").post(addThought);
@@ -13,6 +17,6 @@ router.route("/:userId").post(addThought);
 router.route("/:userId/:thoughtId").put(addReaction).delete(removeThought);
 
 // /api/thoughts/<userId>/<thoughtId>/<reactionId>
-router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
+router.route("/:userId/:thoughtId/:reactionId").delete(removeReaction);
 
 module.exports = router;
